d20646966132c355f447b08e2283920e20e01739
Gabx-dev <58875500+Gabx-dev@users.noreply.github.com>
2022-09-01 13:51:25 -0300

    Feat: Add token authentication

--- Product/tests/test_viewsets/test_product_viewset.py
+++ Product/tests/test_viewsets/test_product_viewset.py
@@ -2,19 +2,25 @@ import json
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
+from rest_framework.authtoken.models import Token
 
 from Product.factories import ProductFactory, CategoryFactory
 from Product.models import Product
+from Order.factories import UserFactory
 
 
 class TestProductViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
+        self.user = UserFactory()
+        Token.objects.create(user=self.user).save()
         self.category = CategoryFactory()
         self.product = ProductFactory(category=[self.category])
 
-    def test_product(self):
+    def test_list_product(self):
+        token = Token.objects.get(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
         res = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
 
         data = json.loads(res.content)
@@ -36,6 +42,26 @@ class TestProductViewSet(APITestCase):
             data["results"][0]["category"][0]["active"], self.category.active
         )
 
+    def test_product(self):
+        token = Token.objects.get(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
+        res = self.client.get(
+            reverse("product-detail", kwargs={"version": "v1", "pk": self.product.id})
+        )
+
+        data = json.loads(res.content)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(data["id"], self.product.id)
+        self.assertEqual(data["title"], self.product.title)
+        self.assertEqual(data["description"], self.product.description)
+        self.assertEqual(data["price"], self.product.price)
+        self.assertEqual(data["active"], self.product.active)
+        self.assertEqual(data["category"][0]["title"], self.category.title)
+        self.assertEqual(data["category"][0]["description"], self.category.description)
+
+        self.assertEqual(data["category"][0]["active"], self.category.active)
+
     def test_create_product(self):
         category = CategoryFactory()
 
@@ -49,6 +75,8 @@ class TestProductViewSet(APITestCase):
 
         data = json.dumps(product)
 
+        token = Token.objects.get(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
         res = self.client.post(
             reverse("product-list", kwargs={"version": "v1"}),
             data=data,
--- Product/viewsets/category_viewset.py
+++ Product/viewsets/category_viewset.py
@@ -1,8 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import (
+    BasicAuthentication,
+    SessionAuthentication,
+    TokenAuthentication,
+)
+from rest_framework.permissions import IsAuthenticated
+
 from Product.models import Category
 from Product.serializers import CategorySerializer
 
 
 class CategoryViewSet(ModelViewSet):
+    authentication_classes = [
+        BasicAuthentication,
+        SessionAuthentication,
+        TokenAuthentication,
+    ]
+    permission_classes = [IsAuthenticated]
     serializer_class = CategorySerializer
     queryset = Category.objects.all().order_by("id")
